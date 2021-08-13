60ce46b5acad062ecb0f4dba5a8fdf4fd71115b5
andrewnturner <andrewturner397@gmail.com>
2021-08-13 23:27:49 +0100

    interpolation

--- src/util/perlin.rs
+++ src/util/perlin.rs
@@ -30,13 +30,25 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Point3) -> f32 {
-        let i = (((4.0 * p.x) as isize) & 255) as usize;
-        let j = (((4.0 * p.y) as isize) & 255) as usize;
-        let k = (((4.0 * p.z) as isize) & 255) as usize;
+        let (i, u) = integer_and_fractional(p.x);
+        let (j, v) = integer_and_fractional(p.y);
+        let (k, w) = integer_and_fractional(p.z);
 
-        let c = self.permutation_x[i] ^ self.permutation_y[j] ^ self.permutation_z[k];
+        let mut c = [[[0.0; 2]; 2]; 2];
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    let r = self.permutation_x[((i + di) & 255) as usize];
+                    c[di as usize][dj as usize][dk as usize] = self.random_floats[
+                        self.permutation_x[((i + di) & 255) as usize] ^
+                        self.permutation_y[((j + dj) & 255) as usize] ^
+                        self.permutation_z[((k + dk) & 255) as usize]
+                    ];
+                }
+            }
+        }
 
-        self.random_floats[c]
+        trilinear_interpolate(c, u, v, w)
     }
 
     fn make_permutation() -> [usize; POINT_COUNT] {
@@ -59,6 +71,28 @@ impl Perlin {
     }
 }
 
+fn integer_and_fractional(x: f32) -> (isize, f32) {
+    (x.floor() as isize, x - x.floor())
+}
+
+fn trilinear_interpolate(c: [[[f32; 2]; 2]; 2], u: f32, v: f32, w: f32) -> f32 {
+    let mut acc = 0.0;
+    for i in 0..2 {
+        for j in 0..2 {
+            for k in 0..2 {
+                acc += (
+                    ((i as f32 * u) + ((1.0 - i as f32) * (1.0 - u))) *
+                    ((j as f32 * v) + ((1.0 - j as f32) * (1.0 - v))) *
+                    ((k as f32 * w) + ((1.0 - k as f32) * (1.0 - w))) *
+                    c[i][j][k]
+                )
+            }
+        }
+    }
+
+    acc
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -74,5 +108,52 @@ mod tests {
         let x = perlin.noise(&Point3::new(1.0, 2.0, 3.0));
 
         assert!((0.0 <= x) && (x <= 1.0));
+    }
+
+    #[test]
+    fn trilinear_interpolate_top_left() {
+        let c = [
+            [[1.0, 2.0], [3.0, 4.0]],
+            [[5.0, 6.0], [7.0, 8.0]],
+        ];
+
+        assert_eq!(
+            trilinear_interpolate(c, 0.0, 0.0, 0.0),
+            1.0,
+        )
+    }
+
+    #[test]
+    fn trilinear_interpolate_bottom_right() {
+        let c = [
+            [[1.0, 2.0], [3.0, 4.0]],
+            [[5.0, 6.0], [7.0, 8.0]],
+        ];
+
+        assert_eq!(
+            trilinear_interpolate(c, 1.0, 1.0, 1.0),
+            8.0,
+        )
+    }
+
+    #[test]
+    fn trilinear_interpolate_centre() {
+        let c = [
+            [[1.0, 2.0], [3.0, 4.0]],
+            [[5.0, 6.0], [7.0, 8.0]],
+        ];
+
+        assert_eq!(
+            trilinear_interpolate(c, 0.5, 0.5, 0.5),
+            4.5,
+        )
+    }
+
+    #[test]
+    fn integer_and_fractional_negative() {
+        assert_eq!(
+            integer_and_fractional(-2.5),
+            (-3, 0.5),
+        );
     }
 }
